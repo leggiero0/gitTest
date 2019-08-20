7b25eb8c4f7e26152abbfc86baa18be0e3146d87
Hyunhee <Hyunhee@DESKTOP-HH>
2019-08-20 13:38:17 +0900

    main수정 출력문, name 변수추가 -com.git.run.Main.java-

--- gitTest/src/com/git/run/Main.java
+++ gitTest/src/com/git/run/Main.java
@@ -3,7 +3,9 @@ package com.git.run;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("안녕 내가 만든 출력이야");
+		String name = "유병승";
 
 	}
 
